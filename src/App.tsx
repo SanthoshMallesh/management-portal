698f404c493f0c8e06eafc76bf1aa0da8460b616
Santhosh M <santhoshm854@gmail.com>
2021-06-14 20:16:57 +0530

    Added react app file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
